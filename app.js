45c78f738db6527cb7e752c08ab35b712fe9f183
Thomas <90297565+htoo123456@users.noreply.github.com>
2022-04-20 08:57:20 +0630

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 const hamburgerMenu = document.querySelector(".hamburgerMenu");
 const navliston = document.querySelector(".nav-list-on");
-let animation = $("#contact").waypoint(function () {
-  console.log("I'm Here");
-  $("#contact").addClass("white");
-});
+// let animation = $("#service").waypoint(function () {
+//   const circleImages = document.querySelectorAll(".service .circle-image")
+//   circleImages.forEach(img => {
+//     img.classList.add("rotate-center")
+//   })
+// });
 
 hamburgerMenu.addEventListener("click", () => {
   if (hamburgerMenu.classList.contains("on")) {
     document.querySelector(".first").classList.remove("first-rotate");
     document.querySelector(".second").classList.remove("second-hide");
     document.querySelector(".third").classList.remove("third-rotate");
-    document.querySelector(".nav-list-on").style.opacity = "-1";
-    document.querySelector(".nav-list-on").style.height = "0";
+    document.querySelector(".nav-list-on").classList.remove("slide-in");
+    document.querySelector(".nav-list-on").classList.add("slide-out");
     hamburgerMenu.classList.remove("on");
     document.querySelector("body").style.overflowY = "visible";
   } else {
     document.querySelector(".first").classList.add("first-rotate");
     document.querySelector(".second").classList.add("second-hide");
     document.querySelector(".third").classList.add("third-rotate");
-    document.querySelector(".nav-list-on").style.height = "100vh";
-    document.querySelector(".nav-list-on").style.opacity = "1";
+    document.querySelector(".nav-list-on").classList.add("slide-in");
+    document.querySelector(".nav-list-on").classList.remove("slide-out");
     document.querySelector("body").style.overflowY = "hidden";
     hamburgerMenu.classList.add("on");
     window.addEventListener("click", (e) => {
@@ -27,8 +29,8 @@ hamburgerMenu.addEventListener("click", () => {
         document.querySelector(".first").classList.remove("first-rotate");
         document.querySelector(".second").classList.remove("second-hide");
         document.querySelector(".third").classList.remove("third-rotate");
-        document.querySelector(".nav-list-on").style.opacity = "-1";
-        document.querySelector(".nav-list-on").style.height = "0";
+        document.querySelector(".nav-list-on").classList.remove("slide-in");
+        document.querySelector(".nav-list-on").classList.remove("slide-out");
         hamburgerMenu.classList.remove("on");
         document.querySelector("body").style.overflowY = "visible";
       }
